03ebe3031e31dfd4ff0aa3cf8f63155a4fb8bcf1
roger <feihu.roger@gmail.com>
2013-05-17 19:56:49 +0800

    convert  chinese char from gbk -> utf-8

--- example/runmain.py
+++ example/runmain.py
@@ -16,7 +16,7 @@ except:
 
 from demoapp import aclmap
 
-# spawn = 30£¬×î¶à30¸öÁ´½Ó£¬·ñÔò»á±¨ too many connections
+# spawn = 30ï¼Œæœ€å¤š30ä¸ªé“¾æ¥ï¼Œå¦åˆ™ä¼šæŠ¥ too many connections
 # spawn = 30, max connction is 30, if greater than this, error is too many connections
 # activetimeout  = 30 ,just for test, common default is 1800s
 server = RPCServer(('0.0.0.0', 3308), aclmap, log=None, spawn=30, webport=8308, querytimeout=20, interval=3, activetimeout=30)
